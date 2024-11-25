962aa30c563ad4a50e3084f5d0271c6cd73f4196
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-11-25 20:13:46 +0530

    done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,8 +6,12 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <Navbar expand="lg" className="bg-primary " variant="dark">
       <Container>
@@ -36,7 +40,14 @@ const Header = () => {
             <NavDropdown title="Rajasva Raj" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.3">My Profile</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => {
+                  localStorage.removeItem("userInfo");
+                  navigate("/");
+                }}
+              >
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
